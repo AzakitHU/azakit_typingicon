1b1237472a7ff6d3e96a5a46bbccadb45f2f4fd2
Azakit <155274165+AzakitHU@users.noreply.github.com>
2024-06-26 20:51:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.Height = 1
 Config.Colour = { r = 5, g = 215, b = 40, a = 185 } -- Icon Color
 Config.Text = "[...]" -- Icon
-Config.Distance = 5 -- the distance within which another player can see the icon above the typing player's head
+Config.Distance = 10 -- the distance within which another player can see the icon above the typing player's head
 Config.TextFont = 0
 Config.MarkerScale = 2.5 -- Icon size
 Config.Time = 3000 -- 3000 = 3 sec The amount of time the icon is displayed above the player's head
