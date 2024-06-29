5a1c8fbc2cb9210a068a841f13e1192b9ea91beb
Azakit <155274165+AzakitHU@users.noreply.github.com>
2024-06-29 14:26:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,4 +6,8 @@ Config.Text = "[...]" -- Icon
 Config.Distance = 10 -- the distance within which another player can see the icon above the typing player's head
 Config.TextFont = 0
 Config.MarkerScale = 2.5 -- Icon size
-Config.Time = 3000 -- 3000 = 3 sec The amount of time the icon is displayed above the player's head
+Config.Time = 3000 -- 3000 = 3 secThe time until the typing icon appears above the player's head
+
+Config.ShowIDDisplay = true -- Whether the ID display feature is enabled or not
+Config.ShowIDButton = 57 -- press F10
+Config.ShowIDTime = 3000 -- 3000 = 3 sec The amount of time the ID is displayed
